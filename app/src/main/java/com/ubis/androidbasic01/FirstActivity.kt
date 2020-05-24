bab142eeca9ec2a42beb51690a85f6c08886c269
nagasee0526 <roronalove@gmail.com>
2020-05-24 14:02:19 +0900

    돌아가기 누르면 생성된 액티비티 종료시키기

--- app/src/main/java/com/ubis/androidbasic01/FirstActivity.kt
+++ app/src/main/java/com/ubis/androidbasic01/FirstActivity.kt
@@ -1,12 +1,23 @@
 package com.ubis.androidbasic01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class FirstActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        goBackBtn.setOnClickListener {
+//  액티비티 하나더 얹어 주는것
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+            finish() // 현재 액티비티 종료
+
+        }
     }
 }
